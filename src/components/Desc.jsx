77450a822d51067d374b9224b006aa970c415953
Rishav Rakesh Thapliyal <rishabhthapliyal03@gmail.com>
2023-05-21 15:27:00 +0530

    Added the desc box

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,57 +1,50 @@
 import { useState } from "react";
 import "./Project.css";
 
-export default function Desc() {
+export default function Desc({img,onModal,name,tags,desc,repo}) {
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-overlay">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl grid_container shadow-lg">
-          <img
-            src="src\assets\tic.png"
-            alt=""
-            className="h-full object-cover"
-          />
-          {/*content*/}
-          <div className=" relative flex flex-col w-full outline-none focus:outline-none bg-background-color">
-            {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold text-title-color">
-                Modal Title
-              </h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-title-color opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => setShowModal(false)}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
+      <div className="overlay">
+        <div className="modal_container">
+          <div className="modal_img">
+            <img
+              src={img}
+              className="w-full h-full object-cover"
+              alt="image"
+              loading="lazy"
+            />
+          </div>
+          <div className="modal_body">
+            <header className="modal_title_container">
+              <div className="modal_title">
+                {name}
+                <span onClick={()=>{onModal(false)}} className="cross">&#10005;</span>
+              </div>
+              <div className="modal_title_meta">
+                {tags.map((it)=>{
+                  return(
+                    <p className="tags">{it}</p>
+                  )
+                })}
+              </div>
+            </header>
+            <div className="about">
+              <ul className="about_list">
+                {desc.map(l=>{
+                  return(
+                    <li>{l}</li>
+                  )
+                })}
+              </ul>
             </div>
-            {/*body*/}
-            <div className="relative p-6 flex-auto">
-              <p className="my-4 text-title-color text-lg leading-relaxed"></p>
-            </div>
-            {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
-              >
-                Close
-              </button>
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
-              >
-                Save Changes
-              </button>
+            <div className="footer">
+              <a className="code" href={repo}>
+                Code
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
     </>
   );
 }
--- src/projects.js
+++ src/projects.js
@@ -3,71 +3,55 @@ import bankimg from "./assets/bank.png";
 import chatimg from "./assets/chatapp.png";
 import movieimg from "./assets/movie.png";
 
-
 export const projects = [
-    {
-        name: "Mulitplayer Tic Tac Toe Game",
-        features: [
-            "Play 1v1 with other player",
-            "Play with computer",
-            "Tournament Mode",
-            "Ability to play with same player again"
-        ],
-        tags: [
-            "ReactJs",
-            "Typescript",
-            "Go"
-        ],
-        src: ticimg,
-        git: "https://github.com/rishav2803/Multiplayer_Tic_Tac_Toe"
+  {
+    name: "Mulitplayer TTT ",
+    features: [
+      "Developed front-end user experience using React.JS.",
+      "Developed back-end using Go(Golang) integrated with gorilla websockets.",
+      "Implemented the React Router DOM to allow nested RESTful routing.",
+      "Provided user with 3 modes of playing (Tournament,multiplayer,vsComputer)",
+    ],
+    tags: ["ReactJs", "Typescript", "Go"],
+    src: ticimg,
+    git: "https://github.com/rishav2803/Multiplayer_Tic_Tac_Toe",
+  },
+  {
+    name: "WeTalk",
+    features: [
+      "Developed front-end with ReactJs.",
+      "Used Nodejs(ExpressJs) for registering and authenticating the users.",
+      "Integrated with socketio to provide the realtime chatting experience.",
+      "Used mysql as the database for storing the messages and for registering users.",
+      "Clean and interactive UI",
+    ],
+    tags: ["ReactJs", "NodeJs", "mysql"],
+    src: chatimg,
+    git: "https://github.com/rishav2803/chat_application",
+  },
+  {
+    name: "MovieCraze",
+    features: [
+      "Used EJS for dynamically updating the HTML markup",
+      "Used Nodejs(ExpressJs) and mysql for registering and authenticating the users.",
+      "Integrated with moviesdb api for fetching top 20 movies and series",
+      "Integrated with chatgpt api for providing sentiment analysis",
+    ],
+    tags: ["EJS", "NodeJs", "mysql"],
+    src: movieimg,
+    git: "https://github.com/rishav2803/Movie-review-",
+  },
+  {
+    name: "Bank Management System",
+    features: [
+      "Registration of the users",
+      "Transfering of money",
+      "Transfering history",
+      "Authentication",
+    ],
+    tags: ["Bootstrap", "NodeJs", "mysql"],
+    src: bankimg,
+    git: "https://github.com/rishav2803/Multiplayer_Tic_Tac_Toe",
+  },
+];
 
-    },
-    {
-        name: "Chat Application",
-        features: [
-            "Registration and authentication of the users",
-            "Chat with the currently online users",
-            "Chat with the Recently contacted users",
-            "Offline Chatting",
-        ],
-        tags: [
-            "ReactJs",
-            "NodeJs",
-            "mysql",
-        ],
-        src: chatimg,
-        git: "https://github.com/rishav2803/chat_application"
-    },
-    {
-        name: "Movie and Series Review System",
-        features: [
-            "Registration and authentication of the users",
-            "User can give rating based on different parameter given",
-            "Current top movies and series are shown",
-            "Movies are fetched from external api"
-        ],
-        tags: [
-            "EJS",
-            "NodeJs",
-            "mysql",
-        ],
-        src: movieimg,
-        git: "https://github.com/rishav2803/Movie-review-"
-    },
-    {
-        name: "Bank Management System",
-        features: [
-            "Registration of the users",
-            "Transfering of money",
-            "Transfering history",
-            "Authentication",
-        ],
-        tags: [
-            "Bootstrap",
-            "NodeJs",
-            "mysql",
-        ],
-        src: bankimg,
-        git: "https://github.com/rishav2803/Multiplayer_Tic_Tac_Toe"
-    },
-]
